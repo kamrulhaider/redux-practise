2688a9ec2a3da11d095a145585253da8ad08f7af
kamrulhaider <kamrulhaider.ssb@gmail.com>
2022-06-05 10:34:17 +0600

    Action and reducer added

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("form index.js");
-console.log("form index.js");
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const initialValue = {
+  numOfCakes: 10,
+};
+// (previousState, action)=> newState
+
+const reducer = (state = initialValue, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default: {
+      return state;
+    }
+  }
+};
